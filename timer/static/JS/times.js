dd00d01808bb5298ba40f00b3f60dad9c2eab8b9
Viktor.Velikov <vvelikov1988@gmail.com>
2020-06-22 16:32:55 -0700

    timerone

--- timer/static/JS/times.js
+++ timer/static/JS/times.js
@@ -2,4 +2,8 @@ var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
+// alert ("this is an alert")
 console.log(dateTime)
+var spantag = document.getElementById("timer");
+spantag.innerHTML = dateTime
+console.log(spantag)
